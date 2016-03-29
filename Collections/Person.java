2437f11cf0affa3e7f5320d0963900b965d0b286
EllenJ <something2buy1@gmail.com>
2016-03-29 15:43:37 -0400

    assignment2

--- Collections/Person.java
+++ Collections/Person.java
@@ -6,26 +6,31 @@ public class Person implements Comparable<Person> {
 	
 	String fName;
 	String lName;
-	
-	
-	public String getfName() {
-		return fName;
-	}
-	public void setfName(String fName) {
+	public Person(String fName, String lName) {
+		super();
 		this.fName = fName;
-	}
-	public String getlName() {
-		return lName;
-	}
-	public void setlName(String lName) {
 		this.lName = lName;
 	}
-	public int compareTo(Person o) {
-		
-		return this.getfName().compareTo(o.getfName());
-		
-	}
 	
+	
+//	public String getfName() {
+//		return fName;
+//	}
+//	public void setfName(String fName) {
+//		this.fName = fName;
+//	}
+//	public String getlName() {
+//		return lName;
+//	}
+//	public void setlName(String lName) {
+//		this.lName = lName;
+//	}
+//	public int compareTo(Person o) {
+//		
+//		return this.getfName().compareTo(o.getfName());
+//		
+//	}
+//	
 	}
 		
 
